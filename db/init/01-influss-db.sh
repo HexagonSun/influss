f6ab86835489f3d96765f56ca25b5a6cc151fb33
HexagonSun <HexagonSun@users.noreply.github.com>
2018-11-16 10:06:50 +0100

    Bootstrap schema & table
    * Specify DB to connect to using "--database"
    * fix defining the default search_path
    ** set search_path on database-level, too
    * Create a schema & table
    * Add some data into the newly created table

--- db/init/01-influss-db.sh
+++ db/init/01-influss-db.sh
@@ -46,17 +46,40 @@ Aborting."
 # Performs the initialization in the already-started PostgreSQL
 # using the preconfigured POSTGRE_USER user.
 init_user_and_db() {
-  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-     CREATE USER $INFLUSS_DB_USER WITH PASSWORD '$INFLUSS_DB_PASSWORD';
-     CREATE DATABASE $INFLUSS_DB_DATABASE;
-     GRANT ALL PRIVILEGES ON DATABASE $INFLUSS_DB_DATABASE TO $INFLUSS_DB_USER;
+  echo "~~~~~~~~~ INIT 1: create user/schema ~~~~~~~~~~~"
+  psql -v ON_ERROR_STOP=1 \
+       -v VERBOSITY=verbose \
+       --username "$POSTGRES_USER" \
+       --dbname=$INFLUSS_DB_DATABASE <<-EOSQL
 
      -- https://www.postgresql.org/docs/9.3/ddl-schemas.html
      -- restrict/remove access on public schema
      REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 
      -- set default search path to the one-and-only schema we use
-     ALTER ROLE $INFLUSS_DB_USER SET search_path = '"$INFLUSS_DB_DATABASE"';
+     ALTER ROLE $INFLUSS_DB_USER SET search_path = $INFLUSS_DB_DATABASE;
+     ALTER DATABASE $INFLUSS_DB_DATABASE SET search_path TO $INFLUSS_DB_DATABASE;
+EOSQL
+
+  echo "~~~~~~~~~ INIT 2: create MESSAGE ~~~~~~~~~~~"
+  psql -v ON_ERROR_STOP=1 \
+       -v VERBOSITY=verbose \
+       --username "$POSTGRES_USER" \
+       --dbname=$INFLUSS_DB_DATABASE <<-EOSQL
+     CREATE SCHEMA $INFLUSS_DB_DATABASE;
+
+     CREATE TABLE $INFLUSS_DB_DATABASE.MESSAGE (
+    ID SERIAL PRIMARY KEY not null,
+    TEXT varchar(1024) not null,
+    VALID_FROM date not null,
+    INVALID_FROM date not null,
+    AUTHOR varchar(128) not null,
+    CREATED_AT date not null,
+    MODIFIED_AT date not null,
+    DELETED bit
+);
+
+CREATE INDEX ON $INFLUSS_DB_DATABASE.MESSAGE (VALID_FROM, INVALID_FROM);
 EOSQL
 }
 
