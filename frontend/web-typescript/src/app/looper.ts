28f9d58bee69dd392f3908d0e30b9fc7586ccb0e
HexagonSun <hexagonsun@users.noreply.github.com>
2018-11-24 23:54:30 +0100

    Added utility class for (asynchronously) executing methods

---
+++ frontend/web-typescript/src/app/looper.ts
@@ -0,0 +1,24 @@
+/* tslint:disable-next-line:no-any */
+type WaitResult= (res: any) => number;
+
+export class Looper {
+
+    public constructor(interval: number, callback: () => void) {
+        this.loop(interval, callback)
+            .catch((err: Error) => {
+                console.error('Error running looper', err);
+            });
+    }
+
+    private readonly loop = async (interval: number, callback: () => void): Promise<void> => {
+        while (true) {
+            await this.wait(interval);
+            callback();
+        }
+    }
+
+    private readonly wait = async (ms: number): Promise<WaitResult> =>
+        /* tslint:disable-next-line:no-any */
+        new Promise<WaitResult>((res: any): number => window.setTimeout(res, ms))
+
+}
