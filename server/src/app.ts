f0a43c44f2e32d55b5766f929dd313331fc64679
HexagonSun <hexagonsun@users.noreply.github.com>
2018-11-30 11:37:10 +0100

    Allow CORS requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,12 +19,22 @@ class App {
     }
 
     private config(): void{
+        this.configureCORS();
         // support application/json type post data
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
 
+    private configureCORS(): void {
+        this.app.use(function(req, res, next) {
+          // allow CORS for localhost development
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
+    }
 }
 
 export default new App().app;
