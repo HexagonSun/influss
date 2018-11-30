fd97e36924269bba06a63176eda663c78e6d7e2f
HexagonSun <hexagonsun@users.noreply.github.com>
2018-11-30 10:17:49 +0100

    Backend-API handling
    * Move base-url into http.service
    * Make host/port definable through global variables (with fallback).
      This will allow us to set the URL from the server-side by substituting values in the webserver

--- frontend/web-typescript/src/app/http.service.ts
+++ frontend/web-typescript/src/app/http.service.ts
@@ -1,8 +1,15 @@
+// Global variables from index.html
+declare var INFLUSS_API_HOST: string;
+declare var INFLUSS_API_PORT: string;
 
 class HttpService {
 
-  protected api = async <T extends {}>(url: string): Promise<T> =>
-    fetch(url)
+  private readonly DEFAULT_PORT: number = 5555;
+  private readonly API_HOST: string = INFLUSS_API_HOST || 'localhost';
+  private readonly API_PORT: number = parseInt(INFLUSS_API_PORT, 10) || this.DEFAULT_PORT;
+
+  protected api = async <T extends {}>(path: string): Promise<T> =>
+    fetch(this.fromPath(path))
       .then(async (response: Response): Promise<T> => {
         if (!response.ok) {
           throw new Error(response.statusText);
@@ -15,5 +22,9 @@ class HttpService {
         console.error(error);
         throw error;
       })
+
+  private readonly fromPath = (path: string): string => `${this.baseUrl()}${path}`;
+
+  private readonly baseUrl = (): string => `http://${this.API_HOST}:${this.API_PORT}`;
 
 }
--- frontend/web-typescript/src/app/messageHttp.service.ts
+++ frontend/web-typescript/src/app/messageHttp.service.ts
@@ -3,7 +3,7 @@ import { Message } from './message.model';
 export class MessageHttpService extends HttpService {
 
     public async getMessages(): Promise<Array<Message>> {
-        return this.api('http://localhost:5555/messages')
+        return this.api('/messages')
           .then((res: Array<Message>) => {
               console.log('Received data from backend: ', res);
 
