4d6a5b4326aef4fa3b150e23e078ebe5afb5d7e4
HexagonSun <hexagonsun@users.noreply.github.com>
2018-12-26 15:53:19 +0100

    Allow changing/specifying express.js port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import app from "./app";
-const PORT = 5005;
+const PORT = process.env.INFLUSS_SERVER_API_NODE_PORT != undefined
+	? process.env.INFLUSS_SERVER_API_NODE_PORT
+	: 5005;
 
 app.listen(PORT, () => {
     console.log('Express server listening on port ' + PORT);
