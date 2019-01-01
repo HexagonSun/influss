baa343cfbda96d5ebb67b79410c5abddc6d64959
HexagonSun <hexagonsun@users.noreply.github.com>
2019-01-01 16:36:33 +0100

    Added main and bottom label
    Added layout constraints

--- frontend/InflussSwift/InflussSwift/ViewController.swift
+++ frontend/InflussSwift/InflussSwift/ViewController.swift
@@ -2,12 +2,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var messageLabel: NSTextField!
+    @IBOutlet weak var bottomLabel: NSTextFieldCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        messageLabel.stringValue = "my message"
+        bottomLabel.stringValue = "my label"
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
