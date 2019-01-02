d74356ce0eecf4d40618ecda500e795855c21ff7
HexagonSun <hexagonsun@users.noreply.github.com>
2019-01-02 14:22:59 +0100

    Fetch messages via MessageService in InflussClient's main event loop

--- frontend/InflussSwift/service/InflussClient.swift
+++ frontend/InflussSwift/service/InflussClient.swift
@@ -1,18 +1,33 @@
 import Foundation
 
 class InflussClient {
-
+    let messageService: MessageService
     let messages: [Message] = []
     let fetchDelay: TimeInterval = 5
 
     var looper: Looper!
 
     init() {
+        messageService = MessageService()
         looper = Looper(self.fetchDelay, self)
     }
 
     func processTick() {
         print("processing tick")
+        messageService.getMessages(withCompletion: processMessages)
+    }
+    
+    func processMessages(messages: [Message]?) {
+        guard let messages = messages else {
+            // no messages available
+            return
+        }
+        
+        // TODO: acutally update messages
+        print("Received messages from backend:")
+        for msg in messages {
+            print("\t*** \(msg)")
+        }
     }
 }
 
