51cd36a00b729ded12760aeb4976c59b4fdcb6d1
HexagonSun <hexagonsun@users.noreply.github.com>
2019-01-02 14:20:55 +0100

    Struct model for data received via REST

--- frontend/InflussSwift/service/Message.swift
+++ frontend/InflussSwift/service/Message.swift
@@ -1,3 +1,18 @@
-class Message {
+import Foundation
+
+struct Message : Codable {
+    // be sure to decode with "convertFromSnakeCase"
+    let id: Int
+    let text: String
+    let validFrom: Date
+    let invalidFrom: Date
+    let author: String
+    let createdAt: Date
+    let modifiedAt: Date
+    let deleted: String
     
+    func isDeleted() -> Bool {
+        return "0" == deleted
+    }
+
 }
