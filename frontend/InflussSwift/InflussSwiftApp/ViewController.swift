5d2c014cb8f4002830fdccd7a8637280e93ec2fa
HexagonSun <hexagonsun@users.noreply.github.com>
2019-01-04 14:34:47 +0100

    add outlets to ViewController

--- frontend/InflussSwift/InflussSwiftApp/ViewController.swift
+++ frontend/InflussSwift/InflussSwiftApp/ViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
