622969d044d6fa017de45c16d079e8f5fa504ab5
HexagonSun <hexagonsun@users.noreply.github.com>
2019-01-19 15:05:18 +0100

    Preferences: added controls

--- frontend/InflussSwift/InflussSwift/PreferencesViewController.swift
+++ frontend/InflussSwift/InflussSwift/PreferencesViewController.swift
@@ -2,4 +2,14 @@ import Cocoa
 
 class PreferencesViewController: NSViewController {
 
+    @IBOutlet weak var apiEndpointInput: NSTextField!
+    @IBOutlet weak var apiKeyInput: NSTextField!
+
+    @IBAction func onCancel(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onOK(_ sender: Any) {
+        
+    }
 }
